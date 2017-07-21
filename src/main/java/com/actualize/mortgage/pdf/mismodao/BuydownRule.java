fe0ebe33c0f37124a37c782cde22277eec479395
Shravan Kumar Boragala <shravan.boragala@compugain.com>
2017-07-21 14:54:20 +0530

    handled various cases for Initial Interest Rate in AIR table

--- src/main/java/com/actualize/mortgage/pdf/mismodao/BuydownRule.java
+++ src/main/java/com/actualize/mortgage/pdf/mismodao/BuydownRule.java
@@ -6,11 +6,12 @@ public class BuydownRule extends MISMODataAccessObject {
 	public final String BuydownChangeFrequencyMonthsCount;
 	public final String BuydownDurationMonthsCount;
 	public final String BuydownIncreaseRatePercent;
-	
+	public final Extension extension;
 	public BuydownRule(Element element) {
 		super(element);
 		BuydownChangeFrequencyMonthsCount = getValueAddNS("BuydownChangeFrequencyMonthsCount");
 		BuydownDurationMonthsCount = getValueAddNS("BuydownDurationMonthsCount");
 		BuydownIncreaseRatePercent = getValueAddNS("BuydownIncreaseRatePercent");
+		extension = new Extension((Element)getElementAddNS("EXTENSION"));
 	}
 }
--- src/main/java/com/actualize/mortgage/pdf/mismodao/Other.java
+++ src/main/java/com/actualize/mortgage/pdf/mismodao/Other.java
@@ -6,11 +6,12 @@ public class Other extends MISMODataAccessObject {
 	public final String IntegratedDisclosureEstimatedClosingCostsExpirationTimezoneType;
 	public final String LockExpirationTimezoneType;
 	public final String DocumentSignatureRequiredIndicator;
-	
+	public final String buydownReflectedInNoteIndicator;
 	public Other(Element element) {
 		super(element);
 		IntegratedDisclosureEstimatedClosingCostsExpirationTimezoneType = getValue("gse:IntegratedDisclosureEstimatedClosingCostsExpirationTimezoneType");
 		LockExpirationTimezoneType = getValue("gse:LockExpirationTimezoneType");
 		DocumentSignatureRequiredIndicator = getValue("gse:DocumentSignatureRequiredIndicator");
+		buydownReflectedInNoteIndicator = getValue("gse:buydownReflectedInNoteIndicator");
 	}
 }
